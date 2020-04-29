efad0a35d0ea33d9b47f2e525f792023b408d506
ShaikshaLove <44459273+ShaikshaLove@users.noreply.github.com>
2020-04-29 15:48:33 +0530

    Add files via upload

---
+++ src/main/java/io/app/GreetingUiApplication.java
@@ -0,0 +1,13 @@
+package io.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingUiApplication.class, args);
+	}
+
+}
